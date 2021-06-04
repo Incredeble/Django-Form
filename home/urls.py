0ef10b66d5e7dbc7e6b1c3e37c85bf39b62662c1
Incredeble <vaibhav.18bcs1050@abes.ac.in>
2021-06-04 20:15:35 +0530

    'Day5'

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .import views
 from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns=[
-    path('home/',views.indexView,name="home"),
+    path('',views.indexView,name="home"),
     path('dashboard/',views.dashboardViews,name="dashboard"),
-    path('',LoginView.as_view(),name="login"),
+    path('login',LoginView.as_view(),name="login"),
     path('register/',views.registerViews,name="register"),
     path('logout/',LogoutView.as_view(next_page='login'),name="logout"),
 ]
