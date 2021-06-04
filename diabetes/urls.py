261bf9e9ea33ed86db223330aa6e3a2f1fb5c777
Incredeble <vaibhav.18bcs1050@abes.ac.in>
2021-06-04 19:44:18 +0530

    Day5

--- diabetes/urls.py
+++ diabetes/urls.py
@@ -14,15 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path ,include
 from django.conf.urls import url
 from firstpage import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('^$',views.index,name='homepage'),
-    url('predictdiabetes',views.predictdiabetes,name='predictdiabetes'),
+    url('',include('home')),
 ]
 
 
